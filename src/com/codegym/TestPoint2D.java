bbe490909aceac420c2595f02ef8188b67557c6d
huongtran99 <huongtrannd1999@gmail.com>
2021-08-11 17:30:52 +0700

    first commit

---
+++ src/com/codegym/TestPoint2D.java
@@ -0,0 +1,9 @@
+package com.codegym;
+
+public class TestPoint2D {
+
+    public static void main(String[] args) {
+        Point2D point2D = new Point2D(2, 3);
+        System.out.println(point2D);
+    }
+}
